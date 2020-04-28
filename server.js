e9beeaf7df3982ca9d96bc55b6a9abc12709a5af
Felipe Moraes <em.felipe@gmail.com>
2020-04-27 23:29:08 -0300

    feita integração com banco de dados

--- server.js
+++ server.js
@@ -5,20 +5,18 @@ const server = express();
 //configuração para apresentar arquivos
 server.use(express.static('public'));
 
+//habilitando body do formulário
+server.use(express.urlencoded({ extended: true }));
 
-const donors = [{
-    name: "Felipe Moraes",
-    blood: "O+"
-}, {
-    name: "Mirian Moraes",
-    blood: "O+"
-}, {
-    name: "Elisa Moraes",
-    blood: "O+"
-}, {
-    name: "Ninna Moraes",
-    blood: "AB+"
-}];
+// conexação com banco de dados
+const Pool = require('pg').Pool;
+const db = new Pool({
+    user: 'postgres',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+});
 
 
 //configuração da template engine
@@ -29,9 +27,38 @@ nunjucks.configure("./", {
 });
 
 
-// configurações de apresentação
+
+// renderização da página
 server.get("/", function(req, res) {
-    return res.render("index.html", { donors });
+
+    db.query("SELECT * FROM donors ORDER BY id DESC", function(err, result) {
+        if (err) return res.send("ERRO DE BANCO DE DADOS.")
+
+        const donors = result.rows;
+        return res.render("index.html", { donors });
+    })
+});
+
+//pegar dados do formulário
+server.post("/", function(req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const blood = req.body.blood;
+
+    // verificando preenchimento de todos os campos
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios.");
+    }
+
+    //inserção no banco de dados
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`;
+    const values = [name, email, blood];
+
+    db.query(query, values, function(err) {
+        if (err) return res.send("Erro no banco de dados.")
+
+        return res.redirect("/");
+    });
 });
 
 
